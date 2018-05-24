8ac77de32bae9e7472a8dd049933816e8af394ee
Will Roscoe <1784940+willroscoe@users.noreply.github.com>
2018-05-24 17:12:35 +0100

    Initial commit

---
+++ wp-multi-holding/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="www.matteringpress.org">
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <div class="logo"><img src="images/mattering-press.png"></div>
+</body>
+</html>
